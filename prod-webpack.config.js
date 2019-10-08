a7e395fbbae17cc4eb66fb4b9bd57d030718031b
Matteo Velludini <matteo.velludini@geo-solutions.it>
2019-10-08 14:26:23 +0200

    first commit

---
+++ prod-webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+
+const themeEntries = require('./MapStore2/build/themes.js').themeEntries;
+const extractThemesPlugin = require('./MapStore2/build/themes.js').extractThemesPlugin;
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const paths = {
+    base: __dirname,
+    dist: path.join(__dirname, "dist"),
+    framework: path.join(__dirname, "MapStore2", "web", "client"),
+    code: [path.join(__dirname, "js"), path.join(__dirname, "MapStore2", "web", "client")]
+};
+
+module.exports = require('./MapStore2/build/buildConfig')(
+    {
+        'swm': path.join(__dirname, "js", "app"),
+        'swm-embedded': path.join(__dirname, "MapStore2", "web", "client", "product", "embedded"),
+        'swm-api': path.join(__dirname, "MapStore2", "web", "client", "product", "api")
+    },
+    themeEntries,
+    paths,
+    extractThemesPlugin,
+    true,
+    "/swm/dist/",
+    '.swm',
+    [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'indexTemplate.html'),
+            chunks: ['swm'],
+            inject: true,
+            hash: true
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'embeddedTemplate.html'),
+            chunks: ['swm-embedded'],
+            inject: true,
+            hash: true,
+            filename: 'embedded.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'apiTemplate.html'),
+            chunks: ['swm-api'],
+            inject: 'head',
+            hash: true,
+            filename: 'api.html'
+        })
+    ],
+    {
+        "@mapstore": path.resolve(__dirname, "MapStore2", "web", "client"),
+        "@js": path.resolve(__dirname, "js")
+    }
+);
